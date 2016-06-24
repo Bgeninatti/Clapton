dea35e63309bbe41de76a49623a35ebc13dd7602
Bruno Geninatti <brunogeninatti@gmail.com>
2016-06-24 14:47:59 -0300

    Ajuste de version

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 #Clapton-base
 
 Librería de Python que implementa las funcionalidades básicas de comunicación por puerto serie aplicando el protocolo TKLan.
+
+#Programa de versiones
+
+- 1.0.0: Revision completa de tests
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='ClaptonBase',
-    version='0.8.0',
+    version='0.9.0',
     description='Libreria de Python que implementa las funcionalidades basicas de comunicacion por puerto serie apl icando el protocolo TKLan.',
     author='Bruno Geninatti',
     author_email='bruno@teknotrol.com',

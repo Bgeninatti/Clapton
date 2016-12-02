da78700883dcd85b1f191ff97ec6fc33f3d05415
Bruno Geninatti <bruno@teknotrol.com>
2016-12-02 16:31:14 -0300

    arreglos en activacion y desacrivacion

--- examples/app_write.py
+++ examples/app_write.py
@@ -1,25 +1,21 @@
-import logging, math, argparse, re
+import logging, argparse
 from ClaptonBase import serial_instance, containers
-from ClaptonBase.cfg import END_LINE
 
 parser = argparse.ArgumentParser(description='Indica el archivo que se quiere grabar y en que nodo.')
 parser.add_argument('--node', type=int, dest="lan_dir", help="Dirección del nodo que se quiere grabar.")
 parser.add_argument('--file', type=str, dest="file", help="Nombre del archivo HEX que tiene el programa.")
 
-
-
 args = parser.parse_args()
 logger = logging.getLogger(__name__)
 
-
 ser = serial_instance.SerialInterface()
 node = containers.Node(args.lan_dir, ser)
-node.ser.check_master()
 node.identify()
 node.deactivate_app()
 with open(filename) as file_read:
     for l in file_read.readlines():
         line = containers.AppLine(line=l)
-        node.write_app_line(l)
+        if line.comando == '00':
+            node.write_app_line(line)
 node.activate_app()
 ser.stop()
